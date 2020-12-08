2d3f38b7c9664c92e4f48b1cd14e3306caa811ca
Cvaniak <igna.cwaniak@gmail.com>
2020-12-08 06:14:46 +0100

    Day 8 - first part

--- Day7/main.py
+++ Day7/main.py
@@ -9,88 +9,40 @@ def read_data(file_name):
 
 def part_test():
     data = read_data("test")
-    # assert part_1(data) == 4
-    assert part_2(data) == 32
-    data = read_data("test2")
-    assert part_2(data) == 126
+    assert part_1(data) == 5
+    # assert part_2(data) == 32
+    # data = read_data("test2")
+    # assert part_2(data) == 126
     # assert part_2(data) == 6
-    data = read_data("input")
+    # data = read_data("input")
     # assert part_1(data) == 27
-    assert part_2(data) == 158730
-
-def find_bag(data, st, data1):
-    print(st)
-    # for d in data:
-        # if st == "shiny gold":
-        #     return data1[st]
-        # else:
-    try:
-        print("------- in", st, data[st])
-        if len(data[st]):
-            w = 0
-            # for k in data[st]:
-            #     print(k)
-            for k in data[st]:
-                # print(k)
-                a = find_bag(data, k, data1)
-                print("---", k, data[st][k], a)
-                w += data[st][k]*a + data[st][k]
-                print("-", k, w)
-            print("return ", st, w)
-            return w 
-        else:
-            print("return ", st, 0)
-            return 0
-    except:
-        print("except1")
-        return 0
-
-def find_bag1(data, st):
-    print(st)
-    for d in data:
-        if st == "shiny gold":
-            return 1
-        else:
-            try:
-                if len(data[st]):
-                    return sum([find_bag1(data, k) for k in data[st]])
-                else:
-                    return 0
-            except:
-                return 0
-
+    # assert part_2(data) == 158730
 
 
 def part_1(data):
     print(data)
-    index= 0
-    sl = {}
+    b= []
     for d in data:
-        d = d.split(" ")
-        b = " ".join(d[:2])
-        c = []
-        a = 0
-        # print(d)
-        while True:
-            try:
-                if(len(d)>a+7):
-                    c.append(" ".join(d[5+a:7+a]))
-                    # print(c)
-                    a += 4
-                else:
-                    break
-            except:
-                break
-        sl[b] = c
-    print(sl)
-    print("----------------")
-    
-    index = 0
-    for d in sl:
-       if find_bag(sl, d):
-           index += 1
-
-    return index-1
+        b.append({d.split(" ")[0]: d.split(" ")[1]})
+    print(b)
+    ind = 0
+    acc =0 
+    byl = set()
+    while True:
+        if(ind in byl):
+            break
+        byl.add(ind)
+        for i in b[ind]:
+            print(b[ind][i])
+            if i == "nop":
+                ind += 1
+            elif i == "jmp":
+                ind += int(b[ind][i])
+            else:
+                acc += int(b[ind][i])
+                ind += 1
+    print(acc)
+    return acc
     # print(sum ([find_bag()])
 
         
@@ -118,64 +70,11 @@ def part_1(data):
         #         break
     
 def part_2(data):
-    print(data)
-    index= 0
-    sl1 = {}
-    for d in data:
-        d = d.split(" ")
-        b = " ".join(d[:2])
-        c = 0
-        a = 0
-        print(d)
-        while True:
-            try:
-                if(len(d)>a+7):
-                    c += int(d[4+a])
-                    a += 4
-                else:
-                    break
-            except:
-                break
-        sl1[b] = c
-    
-    
-    print(sl1)
-    index= 0
-    sl = {}
-    for d in data:
-        d = d.split(" ")
-        b = " ".join(d[:2])
-        c = {}
-        a = 0
-        # print(d)
-        # if b == "shiny gold":
-        #     continue
-        while True:
-            try:
-                if(len(d)>a+7):
-                    c[" ".join(d[5+a:7+a])] =int(d[4+a])
-                    # print(c)
-                    a += 4
-                else:
-                    break
-            except:
-                break
-        sl[b] = c
-        print(b, c)
-    print(sl)
-    print("----------------")
-    
-    index = 0
-    # for d in sl:
-    #     print("----")
-    #     if find_bag1(sl, d):
-    #         index += find_bag(sl, d, sl1)
-    print(find_bag(sl,"shiny gold",sl1))
-    return find_bag(sl,"shiny gold",sl1)
+        
 
 
 if __name__ == "__main__":
     part_test()
     data = read_data("input")
-    # print(part_1(data))
-    print(part_2(data))
+    print(part_1(data))
+    # print(part_2(data))
