6f10949719de18ff0755271bcc1dc6c6e4913dd3
Cvaniak <igna.cwaniak@gmail.com>
2021-12-25 17:43:04 +0100

    Day11 small clean up

--- 2021/Day11/main.py
+++ 2021/Day11/main.py
@@ -15,55 +15,12 @@ def read_data(file_name):
 
 def part_test():
     data = read_data("test")
-    # assert part_1(data, 1) == """6594254334
-    # 3856965822
-    # 6375667284
-    # 7252447257
-    # 7468496589
-    # 5278635756
-    # 3287952832
-    # 7993992245
-    # 5957959665
-    # 6394862637
-    # """
-    # assert part_1(data, 2) == """8807476555
-    # 5089087054
-    # 8597889608
-    # 8485769600
-    # 8700908800
-    # 6600088989
-    # 6800005943
-    # 0000007456
-    # 9000000876
-    # 8700006848
-    # """
-    # assert part_1(data, 3) == """0050900866
-    # 8500800575
-    # 9900000039
-    # 9700000041
-    # 9935080063
-    # 7712300000
-    # 7911250009
-    # 2211130000
-    # 0421125000
-    # 0021119000
-    # """
-    # assert part_1(data, 100) == """0397666866
-    # 0749766918
-    # 0053976933
-    # 0004297822
-    # 0004229892
-    # 0053222877
-    # 0532222966
-    # 9322228966
-    # 7922286866
-    # 6789998766
-    # """, 1656
-    assert part_1(data, 100) == 1656
-    assert part_2(data, 100) == 195
+    step, flash_sum = sol(data, 100)
+    assert (step, flash_sum) == (100, 1656)
+    assert sol(data)[0] == 195
 
 
-def part_1(datat, n):
+def sol(datat, n=None):
     data = datat[:]
     for idx, i in enumerate(data):
         data[idx] = [int(x) for x in i]
@@ -78,57 +35,8 @@ def part_1(datat, n):
                     data[y + i][x + j] += 1
                     if data[y + i][x + j] > 9 and (y + i, x + j) not in flashed:
                         foo(y + i, x + j)
-                    #     foo(y+i, x+j, (y+i, x+j))
 
-    r = 0
-    for _ in range(n):
-        flashed = set()
-        sf = []
-        for iidx, i in enumerate(data):
-            for jidx, j in enumerate(i):
-                data[iidx][jidx] += 1
-                if data[iidx][jidx] > 9:
-                    flashed.add((iidx, jidx))
-                    sf.append((iidx, jidx))
-        # pprint(data)
-        d = defaultdict(tuple)
-        for k, h in sf:
-            foo(k, h)
-        # pprint(data)
-        for iidx, i in enumerate(data):
-            for jidx, j in enumerate(i):
-                if j > 9:
-                    r += 1
-                    data[iidx][jidx] = 0
-        # pprint(data)
-        # print()
-
-    w = ""
-    for idx, i in enumerate(data):
-        for j in i:
-            w += str(j)
-        w += "\n"
-    # print(w)
-    return r
-
-
-def part_2(datat, n):
-    data = datat[:]
-    for idx, i in enumerate(data):
-        data[idx] = [int(x) for x in i]
-
-    def foo(y, x):
-        flashed.add((y, x))
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if not (i == 0 and j == 0):
-                    if y + i < 0 or x + j < 0 or y + i > 9 or x + j > 9:
-                        continue
-                    data[y + i][x + j] += 1
-                    if data[y + i][x + j] > 9 and (y + i, x + j) not in flashed:
-                        foo(y + i, x + j)
-                    #     foo(y+i, x+j, (y+i, x+j))
-
+    flash_sum = 0
     for step in range(1000000):
         flashed = set()
         sf = []
@@ -138,29 +46,33 @@ def part_2(datat, n):
                 if data[iidx][jidx] > 9:
                     flashed.add((iidx, jidx))
                     sf.append((iidx, jidx))
-        # pprint(data)
-        d = defaultdict(tuple)
         for k, h in sf:
             foo(k, h)
-        # pprint(data)
         r = 0
         for iidx, i in enumerate(data):
             for jidx, j in enumerate(i):
                 if j > 9:
                     r += 1
                     data[iidx][jidx] = 0
+        flash_sum += r
         if r == 100:
-            # print(step)
-            return step + 1
-        # pprint(data)
+            return (step + 1, flash_sum)
+        if n and step == n - 1:
+            return step + 1, flash_sum
 
-        # print()
-    print(None)
     return None
+
+
+def part_1(datat):
+    return sol(datat, 100)[1]
+
+
+def part_2(datat):
+    return sol(datat)[0]
 
 
 if __name__ == "__main__":
     part_test()
     data = read_data("input")
-    print(part_1(data, 100))
-    print(part_2(data, 100))
+    print(part_1(data))
+    print(part_2(data))
