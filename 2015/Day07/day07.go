5e235946f27c2a0dc28de980414fb8d3000caa33
Cvaniak <igna.cwaniak@gmail.com>
2022-09-04 16:52:13 +0200

    2015: Day07 Go

--- 2015/Day07/day07.go
+++ 2015/Day07/day07.go
@@ -4,120 +4,67 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
-	"math"
 )
 
 var _, _, _, _, _, _ = fmt.Print, ioutil.ReadFile, log.Print, strings.Trim, strconv.Atoi, sort.Ints
 
-type Operation struct {
-    Dest string
-    What string
-    V1 string
-    V2 string
-	Shift int
-}
-
-
 // AND, OR, LSHIFT, RHIFT, Not
-func parse(line string) Operation{
-	splited := strings.Split(line, " ")
-    var result Operation
-    if splited[0] == "NOT" {
-       result = Operation{splited[3], "NOT", splited[1], "", 0}
-       return result
-    } 
-    if len(splited) == 3 {
-        x, err := strconv.Atoi(splited[0])
-        if err != nil {
-            
-            result = Operation{splited[2], "MAP", splited[0], "", 0}
-        } else{
+func compute(value string, m map[string]string, cache map[string]int) int {
+	if val, ok := cache[value]; ok {
+		return val
+	}
 
-            result = Operation{splited[2], "NUM", "", "", x}
-        }
-    }
-    switch splited[1] {
-    case "AND": 
-        result = Operation{splited[4], splited[1], splited[0], splited[2], 0}
-    case "OR":
-        result = Operation{splited[4], splited[1], splited[0], splited[2], 0}
-    case "LSHIFT":
-        x, _ := strconv.Atoi(splited[2])
-        result = Operation{splited[4], splited[1], splited[0], "", x}
-    case "RSHIFT":
-        x, _ := strconv.Atoi(splited[2])
-        result = Operation{splited[4], splited[1], splited[0], "", x}
-    }
- 
-    return result
+	num, err := strconv.Atoi(value)
+	if err == nil {
+		return num
+	}
+
+	var result int
+	data := strings.Split(m[value], " ")
+	switch {
+	case len(data) == 1:
+		result = compute(data[0], m, cache)
+	case data[0] == "NOT":
+		result = (math.MaxUint16) ^ compute(data[1], m, cache)
+	case data[1] == "AND":
+		result = compute(data[0], m, cache) & compute(data[2], m, cache)
+	case data[1] == "OR":
+		result = compute(data[0], m, cache) | compute(data[2], m, cache)
+	case data[1] == "LSHIFT":
+		result = compute(data[0], m, cache) << compute(data[2], m, cache)
+	case data[1] == "RSHIFT":
+		result = compute(data[0], m, cache) >> compute(data[2], m, cache)
+	}
+	cache[value] = result
+	return result
 }
-
-func compute(value string, m map[string]Operation, cache map[string]int) int{
-    if val, ok := cache[value]; ok {
-        return val
-    }
-
-    what := m[value]
-    x, err1 := strconv.Atoi(what.V1)
-    if err1 != nil {
-        x = compute(what.V1, m, cache)
-    }
-    result := -1
-    switch what.What {
-        case "MAP":
-            result = compute(what.V1, m, cache)
-        case "NUM":
-            result = what.Shift
-        case "NOT":
-            result =  (math.MaxUint16)^x
-        case "AND":
-            y, err2 := strconv.Atoi(what.V2)
-            if err2 != nil {
-                y = compute(what.V2, m, cache)
-            }
-            result = x&y
-        case "OR":
-            y, err2 := strconv.Atoi(what.V2)
-            if err2 != nil {
-                y = compute(what.V2, m, cache)
-            }
-            result = x|y
-        case "LSHIFT":
-            result = x<<what.Shift
-        case "RSHIFT":
-            result = x>>what.Shift
-    }
-    cache[value] = result
-    return result
-}
-    
-
 
 func Part1(content string) {
 	result := 0
+	mapa := map[string]string{}
+	cache := map[string]int{}
+
 	splited := strings.Split(content, "\n")
-    mapa := map[string]Operation{}
-    cache := map[string]int{}
-
-
 	for _, str := range splited {
-        if str == "" {
-            continue
-        }
-        ope := parse(str)
-        mapa[ope.Dest] = ope
-    }
+		if str == "" {
+			continue
+		}
+		ope := strings.Split(str, " -> ")
+		mapa[ope[1]] = ope[0]
+	}
 
-    result = compute("a", mapa, cache)
+	result = compute("a", mapa, cache)
 	fmt.Println(result)
-}
 
-func Part2(content string) {
-	result := 0
-	fmt.Println(result)
+	var resultPart2 int
+	cache = map[string]int{}
+	mapa["b"] = strconv.Itoa(result)
+	resultPart2 = compute("a", mapa, cache)
+	fmt.Println(resultPart2)
 }
 
 func main() {
@@ -126,5 +73,4 @@ func main() {
 		log.Fatal(err)
 	}
 	Part1(string(content))
-	Part2(string(content))
 }
