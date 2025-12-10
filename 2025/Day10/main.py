741028c608d8ad29a370a6a634778c605aad0712
Cvaniak <igna.cwaniak@gmail.com>
2025-12-10 20:52:30 +0100

    2025: Day10 - Part 1 only as I will rather write my linear solver than use Z3 this year...

---
+++ 2025/Day10/main.py
@@ -0,0 +1,201 @@
+import argparse
+import bisect
+import collections
+import functools
+import itertools
+import math
+import operator
+from collections import Counter, defaultdict, deque
+from heapq import heappop, heappush
+from itertools import combinations, permutations, product
+from math import lcm
+from typing import NamedTuple
+
+import parse
+from rich import print
+import inspect
+
+debug_space = False
+
+
+class Debuger:
+    levels: set[str] = set()
+    excluded_parts: set[str] = set()
+    state: bool = True
+
+    def _print(self, *args, lvl="", **kwargs):
+        if not self.state:
+            return
+
+        if self.levels and lvl not in self.levels:
+            return
+
+        c1, c2 = inspect.stack()[1].function, inspect.stack()[2].function
+        if c1 in self.excluded_parts or c2 in self.excluded_parts:
+            return
+
+        print(f" ▄▄▄▄▄ {lvl} ▄▄▄▄▄ ")
+        print("  ", end="")
+        print(*args, **kwargs)
+
+    def __getattr__(self, lvl):
+        return functools.partial(self._print, lvl=lvl)
+
+    def __call__(self, *args, **kwargs):
+        self._print(lvl="0", *args, **kwargs)
+
+    def set_levels(self, lvls):
+        if not lvls:
+            return
+        for lvl in lvls.split(","):
+            self.levels.add(lvl)
+
+    def exclude_parts(self, parts):
+        for part in parts:
+            self.excluded_parts.add(f"part_{part}")
+
+    def turn_on_off(self, state):
+        self.state = state
+
+
+dbg = Debuger()
+
+
+def set_debuger():
+    global dbg
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", nargs="?", default=None, help="Debug List")
+    parser.add_argument(
+        "-n", "--nodebug", action="store_false", help="Remove default debug"
+    )
+    parser.add_argument(
+        "-p",
+        "--excludepart",
+        nargs="?",
+        default="",
+    )
+    args = parser.parse_args()
+    dbg.set_levels(args.debug)
+    dbg.turn_on_off(args.nodebug)
+    dbg.exclude_parts(args.excludepart)
+
+
+# ===== ^^ DEBUGER ^^ =====
+
+wsen = {"E": (1, 0), "S": (0, -1), "W": (-1, 0), "N": (0, 1)}
+dir_4 = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+dir_4c = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
+dir_8 = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+
+
+def prod(factors):
+    return functools.reduce(operator.mul, factors, 1)
+
+
+def read_data(file_name):
+    with open(file_name + ".txt", "r", newline=None) as data:
+        data = data.read().splitlines()
+        return data
+
+
+def load_int_lines(data):
+    data = data[:]
+    for idx, i in enumerate(data):
+        data[idx] = [int(x) for x in i]
+    return data
+
+
+def load_ints_seperated(data, sep=","):
+    return list(map(int, data.split(sep)))
+
+
+def parser(data):
+    pattern = "{test} foo {test1:d}"
+    match = parse.search(pattern, data)
+    match.named
+
+
+class Matrix:
+    data: list[list[int]]
+    n: int
+    m: int
+
+    def __init__(self, data) -> None:
+        self.data = data
+        self.n = len(data)
+        self.m = len(data[0])
+
+    def s_get(self, y, x, default=None):
+        if 0 > y or y >= self.n or 0 > x or x >= self.m:
+            return default
+        return self.data[y][x]
+
+    def s_set(self, y, x, value):
+        if 0 > y or y >= self.n or 0 > x or x >= self.m:
+            return
+        self.data[y][x] = value
+
+
+# ===== ^^ PART OF TEMPLATE ^^ =====
+
+
+def part_test():
+    data = read_data("test")
+    assert part_1(data) == 7
+    assert part_2(data) == None
+
+class Line(NamedTuple):
+    lights: int
+    masks: list[int]
+
+def load(data):
+    res = []
+    for line in data:
+        line_data = line.split(" ")
+        light_result = int(str(line_data[0][1:-1]).replace(".", "0").replace("#","1")[::-1], 2)
+        masks = []
+        for nums in line_data[1:-1]:
+            nums = load_ints_seperated(nums[1:-1])
+            x = 0
+            for num in nums:
+                x += 1 << num 
+            masks.append(x)
+        res.append(Line(light_result, masks))
+    return res
+
+def solve(line):
+    q = deque((x, tuple([x])) for x in line.masks)
+    q = deque([(0, tuple())])
+    # for i in range(70):
+    while True:
+        for _ in range(len(q)):
+            value, nums = q.popleft()
+            for x in line.masks:
+                next_value = value ^ x
+                if next_value == line.lights:
+                    return nums+(x,)
+                q.append((next_value, nums+tuple([x])))
+    return -1
+
+def part_1(data):
+    lines = load(data)
+    answer = 0
+    for line in lines:
+        print(bin(line.lights))
+        s = solve(line)
+        answer += len(s)
+        print(s, len(s))
+    return answer
+
+
+def part_2(data):
+    ...
+
+
+if __name__ == "__main__":
+    set_debuger()
+
+    part_test()
+    data = read_data("input")
+    print(part_1(data[:]))
+    print(part_2(data[:]))
